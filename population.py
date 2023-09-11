simulation runs, bug in axis rotation

population.py
@@ -1,7 +1,16 @@
 import math
+from collections import namedtuple
+import numpy as np
 
 from specimen import Specimen
+from fitness_fun import FitnessFunction
+from cross_mutate import Cross, Mutate
+
 from constants import GeneDesc, ROBOT_HAND
+
+
+Fits = namedtuple('Fits', ('f', 's'))
+'''Tuple to hold state of fit specimen. f - fitness, s - specimen'''
 
 
 class Population:
@@ -10,28 +19,29 @@ class Population:
         self._specimen = [Specimen() for _ in range(pop_size)]
         self.fits: list[Specimen] = None  # Top 30% fittest specimen
 
-        self.crossed = False # Check for crossing
-        self.mutated = False # Check for mutation
+        self.crossed = False  # Check for crossing
+        self.mutated = False  # Check for mutation
 
-        self.pop_fitness = None
+        self.pop_fitness: list[Fits] = None
 
     def calc_fits(self):
-        pop_fitness = [(s.fitness, s) for s in self._specimen]
-        pop_fitness = sorted(pop_fitness, key=lambda x: x[0], reverse=True)
+        # Get fitnesses with corresponding specimen
+        pop_fitness = [Fits(s.fitness, s) for s in self._specimen]
+        # Sort specimen by their fitness
+        pop_fitness = sorted(pop_fitness, key=lambda x: x.f, reverse=True)
 
         self.pop_fitness = pop_fitness
 
-        # We only take top 30% fittest, and an even number of fit,
-        # to get even number we divide by two, round to ceil and multipy
-        # by two
-        fit_index = math.ceil((len(self._specimen) // 3) / 2) * 2
+        # We only take top 30% fittest, and an even number of fit, to get
+        # even number we divide by two, round to ceil and multiply by two
+        fit_index = math.ceil((self.pop_size // 3) / 2) * 2
 
-        self.fits = [s for _, s in pop_fitness][:fit_index]
+        self.fits = [fits.s for fits in pop_fitness][:fit_index]
 
         self.crossed = False
         self.mutated = False
 
-    def cross(self):
+    def cross_mutate(self):
         assert self.fits is not None
         assert len(self.fits) % 2 == 0
 
@@ -41,31 +51,41 @@ class Population:
             parent_2 = self.fits[i + 1]
 
             for _ in range(2):  # Two children from the parents
-                child_fingers_genome = CrossFingers.cross_genomes(
-                    parent_1.fingers_genome, parent_2.fingers_genome
+                # Get fitness maps for both parents
+                fit_map_p_1 = FitnessFunction.get_fitness_map(
+                    parent_1.phalanges, parent_1.fingers.shape
                 )
-                child_brain_genome = CrossBrain.cross_genomes(
-                    parent_1.brain_genome, parent_2.brain_genome
-                )
-
-                child_specimen = Specimen(
-                    fingers_genome=child_fingers_genome, brain_genome=child_brain_genome
+                fit_map_p_2 = FitnessFunction.get_fitness_map(
+                    parent_2.phalanges, parent_2.fingers.shape
                 )
 
-                children.append(child_specimen)
+                # Cross the parents to create new child
+                f_g_child, b_g_child = Cross.cross_genomes(
+                    parent_1.fingers,
+                    parent_1.brain.genome,
+                    fit_map_p_1,
+                    parent_2.fingers,
+                    parent_2.brain.genome,
+                    fit_map_p_2,
+                )
 
-        self.specimen = children
+                # Mutate the child's genomes
+                f_g_child, b_g_child = Mutate.mutate(
+                    f_g_child,
+                    b_g_child,
+                    np.random.uniform(-0.05, 0.05),
+                    np.random.uniform(-0.1, 0.1),
+                    0.3,
+                    0.5,
+                )
+
+                child = Specimen(fingers_genome=f_g_child, brain_genome=b_g_child)
+
+                children.append(child)
+
+        self._specimen = children
 
         self.crossed = True
-
-    def mutate(self):
-        # Make sure we only mutate after crossing
-        assert self.crossed and not self.mutated
-
-        for specimen in self._specimen:
-            specimen.fingers_genome = MutateFingers.mutate(specimen.fingers_genome)
-            # specimen.brain_genome = MutateBrain.mutate(specimen.brain_genome)
-
         self.mutated = True
 
     def repopulate(self):

specimen.py
@@ -88,7 +88,7 @@ class Specimen:
     brain = None
     specimen_URDF = None
 
-    _fitness = 0 # Total fitness of the specimen
+    _fitness = 0  # Total fitness of the specimen
 
     prev_arm_angles = []  # Keeps track of previously applied angles for the arm
 
@@ -143,6 +143,7 @@ class Specimen:
             self.fingers = self.fingers_phenome.load_genome(
                 f'fit_specimen/genome_encodings/{generation_id}_fingers_{specimen_id}.pickle'
             )
+            self._fingers_genome = self.fingers_phenome.genome
 
             self.specimen_URDF = f'{generation_id}_{specimen_id}.urdf'
 
@@ -151,6 +152,7 @@ class Specimen:
             self.brain.load_genome(
                 f'fit_specimen/genome_encodings/{generation_id}_brain_{specimen_id}.pickle'
             )
+            self._brain_genome = self.brain.genome
 
             return
 
@@ -229,11 +231,15 @@ class Specimen:
         '''
 
         p.stepSimulation(physicsClientId=p_id)
+
         if action == 'ready_to_pick' or action == 'drop':
             for phalanx in self._phalanges:
                 if phalanx.phalanx_index == 0:
                     # Spread out the fingers
-                    phalanx.output = -np.pi / 4
+                    phalanx.output = np.pi / 4
+                    print('#######################################')
+                    print(self.fingers[phalanx.finger_index, phalanx.phalanx_index])
+                    print('#######################################')
                 else:
                     phalanx.output = 0
         elif action == 'pick' and iteration == 0:
@@ -275,16 +281,10 @@ class Specimen:
 
                 inputs[f_i][p_i] = np.array(phalanx.inputs)
 
-            # print('===============')
-            # print(f'inputs {inputs}')
-
             # Get trajectories
             outputs = self.brain.trajectories(
                 inputs, target_object=target_object, iteration=iteration
             )
-
-            # print(f'outputs {outputs}')
-            # print('===============')
 
             # Populate output
             for phalanx in self._phalanges:
@@ -295,9 +295,6 @@ class Specimen:
 
         link_indices = [p.link_index for p in self._phalanges]
         target_angles = [p.output for p in self._phalanges]
-
-        # print(f'target_angles {target_angles}')
-        # print('===============')
 
         apply_rotation(body_id, link_indices, target_angles, p_id)
 
@@ -409,21 +406,6 @@ class Specimen:
     def fingers_genome(self):
         return self._fingers_genome
 
-    @fingers_genome.setter
-    def fingers_genome(self, fingers_genome):
-        assert isinstance(fingers_genome, np.ndarray)
-
-        self._fingers_genome = fingers_genome
-        self.__init_fingers()
-
     @property
     def brain_genome(self):
         return self._brain_genome
-
-    @brain_genome.setter
-    def brain_genome(self, brain_genome):
-        assert isinstance(brain_genome, list)
-        assert isinstance(brain_genome[0], np.ndarray)
-
-        self._brain_genome = brain_genome
-        self.__init_brain()